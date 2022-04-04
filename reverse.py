3df66102ef3d9c22f98c1a7ce07eb09e0ccdb880
Ashokkumar Ramasamy <ashokkumarindia10@gmail.com>
2022-04-04 22:36:16 +0100

    practice

---
+++ reverse.py
@@ -0,0 +1,11 @@
+l = ['Google', 'Facebook', 'Yahoo', 'Google']
+# if(l.__contains__('Facebook')):
+#     l.remove('Facebook')
+#     l.append('Facebook')
+
+# l.reverse()
+# print(l.count('Google'))
+# print(tuple(l).count('Google'))
+# print(set(l))
+t = tuple(l)
+print(t)
